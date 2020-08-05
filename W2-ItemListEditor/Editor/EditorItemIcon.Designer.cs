4c9213cb875b20f580cc5b0ee50d8bf7c9034114
SeiTbNao - Djunio <djunio_rmf@hotmail.com>
2020-08-05 17:30:50 -0300

    init

---
+++ W2-ItemListEditor/Editor/EditorItemIcon.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace W2ItemListEditor.Editor
+{
+    partial class EditorItemIcon
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EditorItemIcon));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.arquivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salvarItemIconbinToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirItemIconbinToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 38);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(191, 251);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(128, 368);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Limpar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 368);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Salvar Alteração";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 301);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Item Selecionado:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(125, 301);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 331);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "ícone Atual:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(107, 331);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(96, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arquivoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(218, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // arquivoToolStripMenuItem
+            // 
+            this.arquivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirItemIconbinToolStripMenuItem,
+            this.salvarItemIconbinToolStripMenuItem,
+            this.sairToolStripMenuItem});
+            this.arquivoToolStripMenuItem.Name = "arquivoToolStripMenuItem";
+            this.arquivoToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.arquivoToolStripMenuItem.Text = "Arquivo";
+            // 
+            // salvarItemIconbinToolStripMenuItem
+            // 
+            this.salvarItemIconbinToolStripMenuItem.Enabled = false;
+            this.salvarItemIconbinToolStripMenuItem.Name = "salvarItemIconbinToolStripMenuItem";
+            this.salvarItemIconbinToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.salvarItemIconbinToolStripMenuItem.Text = "Salvar ItemIcon.bin";
+            this.salvarItemIconbinToolStripMenuItem.Click += new System.EventHandler(this.salvarItemIconbinToolStripMenuItem_Click);
+            // 
+            // abrirItemIconbinToolStripMenuItem
+            // 
+            this.abrirItemIconbinToolStripMenuItem.Name = "abrirItemIconbinToolStripMenuItem";
+            this.abrirItemIconbinToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.abrirItemIconbinToolStripMenuItem.Text = "Abrir itemIcon.bin";
+            this.abrirItemIconbinToolStripMenuItem.Click += new System.EventHandler(this.abrirItemIconbinToolStripMenuItem_Click);
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sairToolStripMenuItem.Text = "Sair";
+            // 
+            // EditorItemIcon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(218, 400);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "EditorItemIcon";
+            this.Text = "W2 - EditorItemIcon";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem arquivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirItemIconbinToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salvarItemIconbinToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+    }
+}
---
+++ W2-ItemListEditor/Editor/EditorItemIcon.cs
@@ -0,0 +1,158 @@
+﻿using ItemListEditor;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace W2ItemListEditor.Editor
+{
+   
+    public partial class EditorItemIcon : Form
+    {
+        public int itemSelecionado = -1;
+        public STRUCT_ITEMICON ItemIcon = new STRUCT_ITEMICON();
+		public string FilePath = "";
+
+		public EditorItemIcon()
+        {
+            InitializeComponent();
+        }
+		public void LoadItemIcon()
+		{
+			try
+			{
+
+				using (OpenFileDialog find = new OpenFileDialog())
+				{
+					find.Filter = "ItemIcon.bin|ItemIcon.bin";
+					find.Title = "Selecione sua ItemIcon.bin";
+
+					find.ShowDialog();
+
+					if (find.CheckFileExists)
+					{
+						this.FilePath = find.FileName;
+
+						if (File.Exists(this.FilePath))
+						{
+							byte[] temp = File.ReadAllBytes(this.FilePath);
+
+							if (temp.Length != 26000 && temp.Length != 25992)
+							{
+								MessageBox.Show("ItemIcon inválida", "ItemIcon.bin inválida", MessageBoxButtons.OK, MessageBoxIcon.Error);
+								this.LoadItemIcon();
+							}
+							else
+							{
+
+
+								this.ItemIcon = Pak.ToStruct<STRUCT_ITEMICON>(temp);
+
+								if (listBox1.Items.Count > 0)
+									listBox1.Items.Clear();
+
+
+								for (int i = 0; i < this.ItemIcon.Item.Length; i++)
+								{
+									this.listBox1.Items.Add($"{i.ToString().PadLeft(4, '0')}: {this.ItemIcon.Item[i].Icone}");
+								}
+
+
+								salvarItemIconbinToolStripMenuItem.Enabled = true;
+
+							}
+						}
+						else
+						{
+							Environment.Exit(0);
+						}
+					}
+					else
+					{
+						Environment.Exit(0);
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+		}
+		private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            itemSelecionado = listBox1.SelectedIndex;
+            label2.Text = "" + itemSelecionado;
+            if(itemSelecionado != -1 && itemSelecionado < listBox1.Items.Count)
+            {
+                textBox1.Text = "" + ItemIcon.Item[itemSelecionado].Icone;
+            }
+        }
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			if (itemSelecionado != -1 && itemSelecionado < listBox1.Items.Count)
+			{
+				ItemIcon.Item[itemSelecionado].Icone = int.Parse(textBox1.Text);
+
+
+				if (listBox1.Items.Count > 0)
+					listBox1.Items.Clear();
+
+
+
+				for (int i = 0; i < this.ItemIcon.Item.Length; i++)
+				{
+					this.listBox1.Items.Add($"{i.ToString().PadLeft(4, '0')}: {this.ItemIcon.Item[i].Icone}");
+				}
+
+
+
+				listBox1.SelectedIndex = itemSelecionado;
+			}
+		}
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+		private void abrirItemIconbinToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			this.LoadItemIcon();
+		}
+		public void SalvaritemIconBIN()
+		{
+			try
+			{
+				using (SaveFileDialog save = new SaveFileDialog())
+				{
+					save.Filter = "*.bin|*.bin";
+					save.Title = "Selecione onde deseja salvar sua ItemIcon.bin";
+					save.ShowDialog();
+
+					if (save.FileName != "")
+					{
+						byte[] toSave = Pak.ToByteArray(this.ItemIcon);
+						File.Create(save.FileName).Close();
+						File.WriteAllBytes(save.FileName, toSave);
+						MessageBox.Show($"Arquivo {save.FileName} salvo no modo Encode com sucesso!");
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+		}
+		private void salvarItemIconbinToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			this.SalvaritemIconBIN();
+		}
+	}
+}
